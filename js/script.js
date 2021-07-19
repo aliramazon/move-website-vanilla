c901360c814a3dd1c365ffe96d395c28e4b0f0f7
Ali Ramazon <mr.safarali@gmail.com>
2021-07-19 00:02:52 -0600

    Toggle mobile navigatio

--- js/script.js
+++ js/script.js
@@ -10,12 +10,10 @@ function stickyNavigation(entries) {
     let ent = entries[0];
 
     if (ent.isIntersecting === false) {
-        console.log("adding");
         navElement.classList.add("sticky");
     }
 
     if (ent.isIntersecting === true) {
-        console.log("removing");
         navElement.classList.remove("sticky");
     }
 }
@@ -23,3 +21,16 @@ function stickyNavigation(entries) {
 const observer = new IntersectionObserver(stickyNavigation, options);
 
 observer.observe(headerElement);
+
+const menuIcon = document.querySelector("#menu-icon");
+const closeIcon = document.querySelector("#close-icon");
+const mobileNav = document.querySelector(".mobile-nav");
+
+menuIcon.addEventListener("click", function () {
+    mobileNav.style.display = "block";
+});
+
+closeIcon.addEventListener("click", function () {
+    console.log("ohhhhh");
+    mobileNav.style.display = "none";
+});
